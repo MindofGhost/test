71569462518ffad4ae720808dcec83af23231683
MindofGhost <vishnyakov.a.o@edu.mirea.ru>
2018-10-23 22:16:02 +0300

    Update tabun.user.js

--- tabun.user.js
+++ tabun.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Ghost
 // @match        https://tabun.everypony.ru/
 // @downloadURL  https://mindofghost.github.io/test/tabun.user.js
 // @updateURL    https://mindofghost.github.io/test/tabun.user.js
